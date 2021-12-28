3ec7667717c2cc8a2cf2ef3143e624a343958e40
Ani Pole <apole@ritech.co>
2021-12-28 16:52:52 +0100

    ani teston

--- ani.py
+++ ani.py
@@ -1,1 +1,12 @@
-test
+from dateutil import tz
+from datetime import datetime, timezone
+import pandas as pd
+
+def timezone_adjust(dataset, column):
+    changed_time = pd.to_datetime(dataset[column], unit='s')
+    changed_time = [x.to_pydatetime() for x in changed_time]
+    changed_time = [x.replace(tzinfo=timezone.utc) for x in changed_time]
+    LA = tz.gettz('America/Los_Angeles')
+    changed_time = [x.astimezone(LA) for x in changed_time]
+    dataset[column] = changed_time
+
